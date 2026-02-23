a36f3c23c693d5ddfb226e0575a31d47bf3ace30
reeshaiqbal <reeshaiqbal62@gmail.com>
2026-02-23 20:43:06 +0500

    Implemented navigation system with structured navbar, active link highlighting, and React Router setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/products" element={<Products />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Routes>
+    </>
+  );
+};
+
+export default App;
